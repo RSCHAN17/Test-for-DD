dcaba6b53dab1bffc6fd5a8c422c763349ab0937
Raza <rsc1099@gmail.com>
2026-01-29 14:11:35 +0000

    list issue

--- image-chat-bot.py
+++ image-chat-bot.py
@@ -10,8 +10,74 @@ import datetime
 
 
 df=pd.read_csv('spotted-animals.csv')
-ls=['Sika deer', 'Chinese water deer', 'Beaver', 'Rabbit', 'Mountain hare', 'Brown hare', 'Field vole', 'Bank vole', 'Water vole', 'Brown rat', 'Wood mouse', 'House mouse', 'Harvest mouse', 'Hazel dormouse', 'Red squirrel', 'Grey squirrel', 'Fallow deer', 'Red deer', 'Muntjac deer', 'Roe deer', 'Polecat', 'Pine marten', 'Wildcat', 'Weasel', 'Stoat', 'American mink', 'European otter', 'European badger', 'Red fox', 'Alcathoe bat', 'Whiskered bat', 'Serotine', 'Natterer\'s bat', 'Leisler\'s bat', 'Grey long-eared bat', 'Daubenton\'s bat', 'Brandt\'s bat', 'Lesser horseshoe bat', 'Greater horseshoe bat', 'Bechstein\'s bat', 'Barbastelle bat', 'Brown long-eared bat', 'Noctule', 'Common pipistrelle', 'Soprano pipistrelle', 'Nathusius\' pipistrelle', 'Hedgehog', 'Mole', 'Common shrew', 'Pygmy shrew', 'Water shrew', 'Barn owl', 'Short-eared owl', 'Long-eared owl', 'Tawny owl', 'Little owl', 'Swift', 'Kingfisher', 'Green woodpecker', 'Great spotted woodpecker', 'Lesser spotted woodpecker', 'Honey buzzard', 'Goshawk', 'Sparrowhawk', 'Marsh harrier', 'Hen harrier', 'Red kite', 'White-tailed eagle', 'Buzzard', 'Kestrel', 'Merlin', 'Hobby', 'Peregrine', 'Pheasant', 'Grey partridge', 'Red-legged partridge', 'Quail', 'Woodpigeon', 'Stock dove', 'Rock dove', 'Turtle dove', 'Collared dove', 'Cuckoo', 'Great bustard', 'Crane', 'Water rail', 'Corncrake', 'Moorhen', 'Coot', 'Greylag goose', 'Pink-footed goose', 'White-fronted goose', 'Brent goose', 'Canada goose', 'Barnacle goose', 'Mute swan', 'Whooper swan', 'Bewick\'s swan', 'Shelduck', 'Shoveler', 'Gadwall', 'Wigeon', 'Mallard', 'Pintail', 'Teal', 'Eider', 'Common scoter', 'Velvet scoter', 'Goldeneye', 'Goosander', 'Red-breasted merganser', 'Grey heron', 'Bittern', 'Little egret', 'Great white egret', 'Cormorant', 'Shag', 'Gannet', 'Oystercatcher', 'Avocet', 'Lapwing', 'Golden plover', 'Grey plover', 'Ringed plover', 'Little ringed plover', 'Whimbrel', 'Curlew', 'Bar-tailed godwit', 'Black-tailed godwit', 'Turnstone', 'Knot', 'Ruff', 'Sanderling', 'Dunlin', 'Purple sandpiper', 'Woodcock', 'Snipe', 'Redshank', 'Greenshank', 'Green sandpiper', 'Common sandpiper', 'Arctic skua', 'Great skua', 'Guillemot', 'Razorbill', 'Puffin', 'Black-headed gull', 'Common gull', 'Great black-backed gull', 'Herring gull', 'Lesser black-backed gull', 'Kittiwake', 'Little tern', 'Sandwich tern', 'Common tern', 'Arctic tern', 'Raven', 'Carrion crow', 'Hooded crow', 'Rook', 'Jackdaw', 'Magpie', 'Jay', 'Chough', 'Goldfinch', 'Greenfinch', 'Linnet', 'Twite', 'Redpoll', 'Crossbill', 'Bullfinch', 'Hawfinch', 'Chaffinch', 'Brambling', 'Corn bunting', 'Yellowhammer', 'Cirl bunting', 'Reed bunting', 'House sparrow', 'Tree sparrow', 'Grey wagtail', 'Pied wagtail', 'Yellow wagtail', 'Tree pipit', 'Meadow pipit', 'Rock pipit', 'Woodlark', 'Skylark', 'Black redstart', 'Ring ouzel', 'Treecreeper', 'Nuthatch', 'Wren', 'Dipper', 'Starling', 'Spotted flycatcher', 'Pied flycatcher', 'Wheatear', 'Whinchat', 'Stonechat', 'Redstart', 'Nightingale', 'Robin', 'Blackbird', 'Fieldfare', 'Redwing', 'Mistle thrush', 'Song thrush', 'Yellow-browed warbler', 'Grasshopper warbler', 'Firecrest', 'Dartford warbler', 'Chiffchaff', 'Willow warbler', 'Wood warbler', 'Cetti\'s warbler', 'Reed warbler', 'Sedge warbler', 'Lesser whitethroat', 'Whitethroat', 'Garden warbler', 'Blackcap', 'Goldcrest', 'Marsh tit', 'Willow tit', 'Bearded tit', 'Long-tailed tit', 'Coal tit', 'Great tit', 'Blue tit', 'Sand martin', 'Swallow', 'House martin']
-
+ls = [
+    "Sika deer", "Chinese water deer", "Beaver", "Rabbit", "Mountain hare", 
+    "Brown hare", "Field vole", "Bank vole", "Water vole", "Brown rat", 
+    "Wood mouse", "House mouse", "Harvest mouse", "Hazel dormouse", 
+    "Red squirrel", "Grey squirrel", "Fallow deer", "Red deer", 
+    "Muntjac deer", "Roe deer", "Polecat", "Pine marten", "Wildcat", 
+    "Weasel", "Stoat", "American mink", "European otter", "European badger", 
+    "Red fox", "Alcathoe bat", "Whiskered bat", "Serotine", "Natterer's bat", 
+    "Leisler's bat", "Grey long-eared bat", "Daubenton's bat", "Brandt's bat", 
+    "Lesser horseshoe bat", "Greater horseshoe bat", "Bechstein's bat", 
+    "Barbastelle bat", "Brown long-eared bat", "Noctule", "Common pipistrelle", 
+    "Mole", "Water shrew", "Pygmy shrew", "Common shrew", "European hedgehog", 
+    "Sand lizard", "Smooth snake", "Adder", "Grass snake", "Slow worm", 
+    "Common lizard", "Marsh frog", "Common frog", "Natterjack toad", 
+    "Common toad", "Palmate newt", "Smooth newt", "Great crested newt", 
+    "Red-necked grebe", "Great northern diver", "Little grebe", 
+    "Great crested grebe", "Black-necked grebe", "Slavonian grebe", 
+    "Black-throated diver", "Red-throated diver", "Mediterranean gull", 
+    "Sooty shearwater", "Storm Petrel", "Roseate tern", "Arctic tern", 
+    "Common tern", "Sandwich tern", "Little tern", "Kittiwake", 
+    "Great black-backed gull", "Lesser black-backed gull", "Herring gull", 
+    "Common gull", "Black-headed gull", "Arctic skua", "Great skua", 
+    "Razorbill", "Guillemot", "Black guillemot", "Puffin", "Shag", 
+    "Cormorant", "Northern gannet", "Manx shearwater", "Fulmar", 
+    "Red-crested pochard", "Long-tailed duck", "Egyptian goose", "Smew", 
+    "Garganey", "Eider", "Common scoter", "Red-breasted merganser", 
+    "Goosander", "Goldeneye", "Tufted duck", "Pochard", "Teal", "Wigeon", 
+    "Shoveler", "Pintail", "Gadwall", "Mallard", "Mandarin duck", 
+    "Shelduck", "Brent goose", "Barnacle goose", "Canada goose", 
+    "Pink-foot goose", "White-fronted goose", "Greylag goose", 
+    "Whooper swan", "Bewick's swan", "Mute swan", "Glossy ibis", 
+    "Cattle egret", "Common crane", "Great white egret", "European spoonbill", 
+    "Little egret", "Grey heron", "Bittern", "Honey buzzard", "Goshawk", 
+    "Barn owl", "Little owl", "Long-eared owl", "Short-eared owl", "Tawny owl", 
+    "Merlin", "Peregrine falcon", "Hobby", "Kestrel", "Sparrowhawk", 
+    "Buzzard", "Hen harrier", "Marsh harrier", "Osprey", "Red kite", 
+    "Golden eagle", "White-tailed eagle", "Quail", "Capercaillie", 
+    "Ptarmigan", "Pheasant", "Grey partridge", "Red-legged partridge", 
+    "Black grouse", "Red grouse", "Little stint", "Stone curlew", 
+    "Black-winged stilt", "Jack snipe", "Purple sandpiper", "Corncrake", 
+    "Red-neck phalarope", "Snipe", "Woodcock", "Whimbrel", "Curlew", 
+    "Bar-tailed godwit", "Black-tailed godwit", "Greenshank", "Redshank", 
+    "Ruff", "Common sandpiper", "Green Sandpiper", "Dunlin", "Sanderling", 
+    "Knot", "Turnstone", "Grey plover", "Golden plover", "Lapwing", 
+    "Ringed plover", "Little ringed plover", "Avocet", "Oystercatcher", 
+    "Coot", "Moorhen", "Water rail", "Collared dove", "Turtle dove", 
+    "Stock dove", "Woodpigeon", "Rock dove", "Wryneck", "Waxwing", 
+    "Kingfisher", "Cuckoo", "Lesser spotted woodpecker", "Green woodpecker", 
+    "Great spotted woodpecker", "Nightjar", "Swallow", "House martin", 
+    "Sand martin", "Swift", "Ring-necked parakeet", "Tree pipit", 
+    "Shore lark", "Tree sparrow", "House sparrow", "Dunnock", "Grey wagtail", 
+    "Yellow wagtail", "Pied wagtail", "Meadow pipit", "Rock pipit", 
+    "Woodlark", "Skylark", "Black redstart", "Ring ouzel", "Treecreeper", 
+    "Nuthatch", "Wren", "Dipper", "Starling", "Spotted flycatcher", 
+    "Pied flycatcher", "Wheatear", "Whinchat", "Stonechat", "Redstart", 
+    "Nightingale", "Robin", "Blackbird", "Fieldfare", "Redwing", 
+    "Mistle thrush", "Song thrush", "Yellow-browed warbler", 
+    "Grasshopper warbler", "Firecrest", "Dartford warbler", "Chiffchaff", 
+    "Willow warbler", "Wood warbler", "Cetti's warbler", "Reed warbler", 
+    "Sedge warbler", "Lesser whitethroat", "Whitethroat", "Garden warbler", 
+    "Blackcap", "Goldcrest", "Marsh tit", "Willow tit", "Bearded tit", 
+    "Long-tailed tit", "Coal tit", "Blue tit", "Great tit", "Great grey shrike", 
+    "Raven", "Hooded crow", "Carrion crow", "Rook", "Jackdaw", "Chough", 
+    "Jay", "Magpie", "Common rosefinch", "Twite", "Brambling", "Hawfinch", 
+    "Snow bunting", "Corn bunting", "Yellowhammer", "Reed bunting", 
+    "Common crossbill", "Bullfinch", "Siskin", "Goldfinch", "Greenfinch", 
+    "Lesser redpoll", "Linnet", "Chaffinch"
+]
 st.markdown("""
     <style>
     /* Target the text inside */
@@ -82,11 +148,9 @@ if uploaded_file:
             }
         ],
     )
-
     answer=response.output_text
     output=[x.strip() for x in answer.split(",")]
     if output[0]=='yes':
-
         name = df.loc[df['name'] == output[1]]['name'].values[0]
         zoo_id = df.loc[df['name'] == output[1]]['zoo_id'].values[0]
         species = df.loc[df['name'] == output[1]]['species'].values[0]
@@ -94,19 +158,13 @@ if uploaded_file:
         pack_bonus_mult = df.loc[df['name'] == output[1]]['pack_bonus_mult'].values[0]
         num=output[2]
         data={"name":name,"zoo_id":zoo_id,"species":species,"capture_points":capture_points,"how_many":num}
-    
         st.markdown(f'**Spotted:**')
         st.html(f"""<div id='info'>
-        <p>You have spotted: <span id='name'>{data["name"]}
-        </span></p>
-        <p>Zoo ID: <span id='zoo_id'>{data["zoo_id"]}
-        </span></p>
-        <p>Species: <span id='species'>{data['species']}
-        </span></p>           
-        <p>Points: <span id='capture_points'>{data['capture_points']}
-        </span></p>
-        <p>Number of them: <span id='number'>{data['how_many']}
-        </span></p>
+        <p>You have spotted: <span id='name'>{data["name"]}</span></p>
+        <p>Zoo ID: <span id='zoo_id'>{data["zoo_id"]}</span></p>
+        <p>Species: <span id='species'>{data['species']}</span></p>           
+        <p>Points: <span id='capture_points'>{data['capture_points']}</span></p>
+        <p>Number of them: <span id='number'>{data['how_many']}</span></p>
         </div>
         """)
         st.html(f"<span id='img'> <img src='data:image;base64,{base64_image}' style='max-height:300px;'/></span>")
